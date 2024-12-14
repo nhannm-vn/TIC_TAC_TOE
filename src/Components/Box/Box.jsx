5d0f375509a14485d7158c28617685aeb915f546
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2024-12-14 08:27:31 +0700

    update 14-12-2024

---
+++ src/Components/Box/Box.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './box.module.scss'
+
+function Box() {
+  return <button className={styles} onClick={() => {}}></button>
+}
+
+export default Box
---
+++ src/Components/Game/Game.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Game() {
+  return <h1>Tic tac toe</h1>
+}
+
+export default Game
