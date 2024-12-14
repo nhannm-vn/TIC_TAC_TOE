cc32897ccb4d990424e0aeada4968575287db779
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2024-12-14 15:15:20 +0700

    update 14-12-2024

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,88 +1,10 @@
 import React, { useState } from 'react'
 import Board from '../Board/Board'
-import { difference } from 'lodash'
 import Message from '../Message/Message'
 import Button from '../Button/Button'
-import getRandomInt from '../../Functions/getRandomInt'
+import { winPattern } from '../../Constants/winPattern'
+import determineComputerMove from '../../Functions/determineComputerMove'
 
-//_Function giúp xác định các nước cờ của computer
-function determineComputerMove(board, player) {
-  //_Tạo cái mảng kt pos computer
-  const computerMoves = []
-  //_Tạo cái mảng kt post human
-  const humanMoves = []
-  board.forEach((box, index) => {
-    if (box === player.computer) {
-      computerMoves.push(index)
-    }
-    if (box === player.human) {
-      humanMoves.push(index)
-    }
-  })
-
-  //_________If can win, then win
-
-  for (const pattern of winPattern) {
-    //_Đầu tiên kt trong danh sách đánh coi có vô thế nào mà còn 1 nước nữa win không
-    const winPositions = difference(pattern, computerMoves)
-    if (winPositions.length === 1) {
-      //_Nếu như còn đúng một chỗ thì phải kiểm tra xe chỗ đó có ai đánh chưa
-      //nếu chưa thì dứt liền
-      const posWin = winPositions[0]
-      if (!board[posWin]) {
-        //_Nếu chỗ đó chưa ai đánh thì đưa ra để cho đánh liền
-        return posWin
-      }
-    }
-  }
-
-  //_________If cannot win, then block
-
-  for (const pattern of winPattern) {
-    //_Đầu tiên kt trong danh sách đánh coi có vô thế nào mà còn 1 nước nữa
-    //human win thì mình sẽ block vị trí đó
-    const winPositions = difference(pattern, humanMoves)
-    if (winPositions.length === 1) {
-      //_Kiểm tra xem vị trí đó có ai đánh chưa
-      const posWin = winPositions[0]
-      if (!board[posWin]) {
-        //_Nếu chỗ đó chưa ai đánh thì đưa ra để cho đánh liền
-        return posWin
-      }
-    }
-  }
-  //_________If cannot block, take the middle
-
-  //_Nếu không chặn được gì hoặc không có gì để đánh
-  //thì ta nên đi giữa để cơ hội cao hơn
-  const centerSquare = 4
-  //_Nếu chưa ai đánh thì dứt
-  if (!board[centerSquare]) {
-    return centerSquare
-  }
-
-  //_________If cannot middle, then random
-  let randomPosition = getRandomInt(0, 9)
-  //_Nếu đánh ở vị trí bất kỳ mà
-  //vị trí đó k trống thì random vị trí khác
-  while (board[randomPosition]) {
-    randomPosition = getRandomInt(0, 9)
-  }
-  return randomPosition
-}
-
-//_Các trường hợp sẽ win
-//mảng chứa các mảng con chứa các vị trí khi đi vào đó thì sẽ win
-const winPattern = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
 //_Kiểm tra xem như thế nào là win
 const isWon = (board) => (icon) => {
   for (let i = 0; i < winPattern.length; i++) {
---
+++ src/Constants/winPattern.js
@@ -0,0 +1,12 @@
+//_Các trường hợp sẽ win
+//mảng chứa các mảng con chứa các vị trí khi đi vào đó thì sẽ win
+export const winPattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
